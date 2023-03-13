19bd98d461c096a56dd06e6fc13b1d53205b32c0
Nick Valdez <nicholastvaldez@gmail.com>
2023-03-13 10:49:57 -0500

    add q params for genre in sampleview

--- samplstakapi/views/sample.py
+++ samplstakapi/views/sample.py
@@ -25,9 +25,24 @@ class SampleView(ViewSet):
         Returns:
             Response -- JSON serialized list of samples
         """
+        samples = []
         samples = Sample.objects.all()
+        if 'genre' in request.query_params:
+            if request.query_params['genre'] == "pop":
+                samples = samples.filter(genre=1)
+            elif request.query_params['genre'] == "hiphop":
+                samples = samples.filter(genre=2)
+            elif request.query_params['genre'] == "rnb":
+                samples = samples.filter(genre=3)
+            elif request.query_params['genre'] == "lofi":
+                samples = samples.filter(genre=4)
+            elif request.query_params['genre'] == "edm":
+                samples = samples.filter(genre=5)
+        else:
+            samples = Sample.objects.all()
+
         serializer = SampleSerializer(samples, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class SampleSerializer(serializers.ModelSerializer):
