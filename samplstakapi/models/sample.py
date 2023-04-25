7796e6513be8e16a9699ad68e03cd493833ead51
Nick Valdez <nicholastvaldez@gmail.com>
2023-04-25 09:43:22 -0500

    change on delete to protect on sampe model

--- samplstakapi/models/sample.py
+++ samplstakapi/models/sample.py
@@ -10,8 +10,8 @@ class Sample(models.Model):
     file_url = models.FileField(
         upload_to='wav', default='wav/none/no-wav.wav')
     file_name = models.CharField(max_length=100)
-    instrument = models.ForeignKey(Instrument, on_delete=models.DO_NOTHING)
+    instrument = models.ForeignKey(Instrument, on_delete=models.PROTECT)
     genre = models.ManyToManyField(Genre, related_name="genre_samples")
     producer = models.ForeignKey(
         Producer, on_delete=models.CASCADE, related_name="producer_samples")
-    drumkit = models.ForeignKey(Drumkit, on_delete=models.CASCADE, null=True)
+    drumkit = models.ForeignKey(Drumkit, on_delete=models.PROTECT, null=True)
