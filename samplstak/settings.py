4da9df7eb4b19d418897e780a8d5b35da1cbbe5c
Nick Valdez <nicholastvaldez@gmail.com>
2023-06-29 13:01:15 -0500

    add cors whitelist

--- samplstak/settings.py
+++ samplstak/settings.py
@@ -60,7 +60,8 @@ CORS_ORIGIN_WHITELIST = (
     'https://nicholasvaldez.github.io',
     'http://localhost:5173',
     'http://localhost:8000',
-    'http://127.0.0.1'
+    'http://127.0.0.1',
+    'http://127.0.0.1:5173',
 
 
 )
